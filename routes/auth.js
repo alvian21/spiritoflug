6185e103c2bf427b23fdbd483048bee78a72a54d
Alvian <alvian.ardhiansyah@gmail.com>
2020-08-09 12:52:04 +0700

    signin

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,5 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 router.post("/signup", authController.signUp);
-
+router.post("/signin", authController.signIn);
 module.exports = router;
