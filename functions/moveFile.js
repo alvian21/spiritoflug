6d49419db5177a1fa0507d7b39cf01d2aa56486c
Alvian <alvian.ardhiansyah@gmail.com>
2020-08-15 15:09:50 +0700

    kegiatan

--- functions/moveFile.js
+++ functions/moveFile.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = (files, filename, callback) => {
-  files.mv(filename, function(err, res) {
+  files.mv(path.resolve(filename), function(err, res) {
     if (err) {
       return callback(err);
     } else {
